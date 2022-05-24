010643f6aee4691945961bd4ec65c643166f8430
lach8@o2.pl <lach8@o2.pl>
2022-05-24 21:30:52 +0200

    Heruku deploy fix

--- craigslist_app/craigslist_app/settings.py
+++ craigslist_app/craigslist_app/settings.py
@@ -12,14 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 import environ
-import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
 environ.Env.read_env()
-django_on_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -134,3 +132,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login'
+
+import django_heroku
+django_heroku.settings(locals())
