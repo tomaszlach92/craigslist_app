77b5e210c19426805ff1dd296e5dfcb9e8e72730
lach8@o2.pl <lach8@o2.pl>
2022-05-29 16:56:45 +0200

    prepare to aws upload

--- craigslist_app/settings.py
+++ craigslist_app/settings.py
@@ -137,3 +137,10 @@ LOGIN_REDIRECT_URL = '/'
 
 import django_heroku
 django_heroku.settings(locals())
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_QUERYSTRING_AUTH = False
