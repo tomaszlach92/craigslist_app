64ac1843d6ff94ae9b90ee6190db712472a72a49
lach8@o2.pl <lach8@o2.pl>
2022-06-07 15:40:54 +0200

    Configure Announcement Admin view, add fields to display and field to filter by.

--- craigslist/admin.py
+++ craigslist/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from .models import Category, Announcement, Reservation, Transaction
 
 admin.site.register(Category)
-admin.site.register(Announcement)
 admin.site.register(Reservation)
 admin.site.register(Transaction)
+
+
+@admin.register(Announcement)
+class AnnouncementAdmin(admin.ModelAdmin):
+    """
+    Configure Announcement Admin view, add fields to display and field to filter by.
+    """
+    list_display = ('title', 'user_who_added', 'status', 'created_at', 'updated_at',)
+    list_filter = ('status',)
